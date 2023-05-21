72b14ba265bc73e73cf5c74d2b4448c751a06e38
Sherlezy <alesercolani@unibz.it>
2023-05-21 16:23:30 +0200

    added screen Size of the game and Thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("UNIBZ 2d Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null); // display at center of the screen
+        window.setVisible(true);
+
     }
 }
